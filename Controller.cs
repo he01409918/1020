b8be037cdc2d1c3e6158674193784b997c0b189d
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-20 14:21:58 +0800

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * Input.GetAxis("Horizontal") * speed;
+    }
+}
