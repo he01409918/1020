9ff106448c0e9e2ef5f624de120b91a0e64033a0
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-20 13:24:35 +0800

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
