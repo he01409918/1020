788b90b3b1f3b1d2c6c4abf9ad890eed768f5b44
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-27 14:15:35 +0800

    Add files via upload

---
+++ BallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public bool isStart;
+
+    private void Update()
+    {
+        if (isStart  == false)
+        {
+            GameObject Controller = Manager.Instance.Controller;
+            Vector3 offset = new Vector3(0, 2f, 0);
+            transform.position = Controller.transform.position + offset;
+        }
+    }
+}
