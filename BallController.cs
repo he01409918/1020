77fd166e17bfe164c9dcef267a4542a7aed7a3c9
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-27 14:28:40 +0800

    Update BallController.cs

--- BallController.cs
+++ BallController.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     public bool isStart;
+
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         if (isStart  == false)
         {
             GameObject Controller = Manager.Instance.Controller;
-            Vector3 offset = new Vector3(0, 2f, 0);
+            Vector3 offset = new Vector3(0, 0.8f, 0);
             transform.position = Controller.transform.position + offset;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
+                isStart = true;
+            }
         }
     }
+
 }
