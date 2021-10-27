dd574f8396aa888ac50cc10272b9db463ff2b358
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-27 13:43:39 +0800

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance;
+
+    public GameObject Ball;
+    void Start()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        
+    }
+}
