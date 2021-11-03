b951801e5464b5c7b410361c3b21e426c362600b
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-03 13:39:42 +0800

    Add files via upload

---
+++ Brick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Manager.Instance.OnAddBricksList(this.gameObject);
+    }
+
+    public void OnGetHit()
+    {
+        Manager.Instance.OnRemoveBricksList(this.gameObject);
+        Destroy(gameObject);
+    }
+}
